40a5ef98340475260dcbf257a3a5bd0536a410d8
Mahesh kumar <maheshkumaryadav1430@gmail.com>
2023-06-01 21:04:21 +0530

    created demo server with some routes using express, methods:post,get

--- Node JS/node_first_app_practice/app.js
+++ Node JS/node_first_app_practice/app.js
@@ -1,21 +1,56 @@
-let http = require("http");
-const fs = require("fs");
-const server = http.createServer((req, res) => {
-  if (req.url === "/") {
-    res.write("<h1>Welcome to Node js</h1>");
-    return res.end();
+const express = require("express");
+const file = require("fs");
+const app = express();
+app.use(express.json());
+app.use((req, res, next) => {
+  if (req.url === "/post") {
+    if (req.body.name != "") {
+      if (req.body.age >= 18) {
+        return next();
+      } else {
+        return res.send({
+          message: "age must be greater than 18",
+          status: 403,
+        });
+      }
+    } else {
+      return res.send({ message: "name cannot be blank", status: 403 });
+    }
   }
-  if (req.url === "/products") {
-    let data = fs.readFileSync("db.json", { encoding: "utf-8" });
-    let parsedData = JSON.parse(data).products;
-    res.write(JSON.stringify(parsedData));
-    return res.end();
-  }
+  next();
+});
+app.get("/", (req, res) => {
+  let fileData = file.readFileSync("db.json", { encoding: "utf-8" });
+  let parsedData = JSON.parse(fileData);
+  res.send(parsedData);
 });
 
-let port = 8081;
-let host = "localhost";
+app.get("/users/:id", (req, res) => {
+  let { id } = req.params;
+  console.log(id);
+  let fileData = file.readFileSync("db.json", { encoding: "utf-8" });
+  let parsedData = JSON.parse(fileData).data;
 
-server.listen(port, host, () => {
-  console.log(`server is running at PORT: ${port},Link http://${host}:${port}`);
+  let findUser = parsedData.findIndex((user) => user.id == id);
+  if(findUser>-1){
+    res.send(parsedData[findUser]);
+  }else{
+    res.send({message:"user not found",status:500})
+  }
+  console.log(findUser);
 });
+app.post("/post", (req, res) => {
+  let fileData = file.readFileSync("db.json", { encoding: "utf-8" });
+  let parsedData = JSON.parse(fileData);
+  let response = req.body;
+  response.id = parsedData.data.length + 1;
+  parsedData.data.push(response);
+  file.writeFileSync("db.json", JSON.stringify(parsedData), {
+    encoding: "utf-8",
+  });
+  res.send({ status: 200, message: "success" });
+});
+// const server = http.createServer(app);
+// server.listen(3002);
+
+app.listen(8080);
