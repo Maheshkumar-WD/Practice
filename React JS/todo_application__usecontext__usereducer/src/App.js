8408b5a3e95d23a7423370cb157ac52bc711e56e
Mahesh kumar <maheshkumaryadav1430@gmail.com>
2023-06-02 18:48:52 +0530

    updated to initial project

--- React JS/todo_application__usecontext__usereducer/src/App.js
+++ React JS/todo_application__usecontext__usereducer/src/App.js
@@ -1,16 +1,5 @@
-import { useContext } from "react";
-import { TodoContext } from "./context/todoContext";
-
 function App() {
-  let todoContext = useContext(TodoContext);
-  return (
-    <div className="App">
-      <button onClick={()=>{todoContext.addTodo({id:todoContext.todos.length+1})}}>click</button>
-      {todoContext.todos.map((todo,index)=>{
-        return <h1 key={index}>{todo.id}</h1>
-      })}
-    </div>
-  );
+  return <div className="App"></div>;
 }
 
 export default App;
